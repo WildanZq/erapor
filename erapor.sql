871096025c83209d823a2da0e12cf8354f90fe82
Wildan Ziaulhaq <wildan2wildan@gmail.com>
2017-12-09 05:35:43 +0700

    Update db

--- erapor.sql
+++ erapor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 06 Des 2017 pada 08.41
+-- Generation Time: 08 Des 2017 pada 23.34
 -- Versi Server: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -29,15 +29,17 @@ SET time_zone = "+00:00";
 CREATE TABLE `admin` (
   `id_admin` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` text NOT NULL
+  `password` text NOT NULL,
+  `nama_admin` varchar(100) NOT NULL,
+  `foto_admin` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `admin`
 --
 
-INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama_admin`, `foto_admin`) VALUES
+(1, 'admin', 'admin', 'Admin Satu', NULL);
 
 -- --------------------------------------------------------
 
@@ -49,16 +51,34 @@ CREATE TABLE `guru` (
   `id_guru` int(11) NOT NULL,
   `nik` varchar(50) NOT NULL,
   `nama_guru` varchar(100) NOT NULL,
+  `jk` int(1) NOT NULL,
   `foto_guru` text,
-  `password` text NOT NULL
+  `password` text NOT NULL,
+  `telp` varchar(20) DEFAULT NULL,
+  `alamat` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `guru`
 --
 
-INSERT INTO `guru` (`id_guru`, `nik`, `nama_guru`, `foto_guru`, `password`) VALUES
-(1, 'nik1', 'Zxc Vbn', NULL, 'nik1');
+INSERT INTO `guru` (`id_guru`, `nik`, `nama_guru`, `jk`, `foto_guru`, `password`, `telp`, `alamat`) VALUES
+(1, 'nik1', 'Zxc Vbn', 1, NULL, 'nik1', NULL, NULL),
+(2, 'nik2', 'Asd Fgh', 0, NULL, 'nik2', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kd`
+--
+
+CREATE TABLE `kd` (
+  `id_kd` int(11) NOT NULL,
+  `nama_kd` varchar(50) NOT NULL,
+  `urutan` int(5) NOT NULL,
+  `semester` int(1) NOT NULL,
+  `id_mapel` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -68,22 +88,56 @@ INSERT INTO `guru` (`id_guru`, `nik`, `nama_guru`, `foto_guru`, `password`) VALU
 
 CREATE TABLE `kelas` (
   `id_kelas` int(11) NOT NULL,
-  `nomor_kelas` varchar(5) NOT NULL,
-  `jurusan` varchar(10) NOT NULL,
-  `angkatan` int(11) NOT NULL
+  `id_kelompok_kelas` int(11) NOT NULL,
+  `nama_kelas` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `id_kelompok_kelas`, `nama_kelas`) VALUES
+(1, 1, 'X RPL 1'),
+(2, 2, 'X TKJ 1'),
+(3, 1, 'X RPL 2'),
+(4, 2, 'X TKJ 2');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `log`
+-- Struktur dari tabel `kelompok_kelas`
 --
 
-CREATE TABLE `log` (
-  `id_log` int(11) NOT NULL,
-  `id_guru` int(11) DEFAULT NULL,
-  `action` text NOT NULL
+CREATE TABLE `kelompok_kelas` (
+  `id_kelompok_kelas` int(11) NOT NULL,
+  `nama_kelompok_kelas` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kelompok_kelas`
+--
+
+INSERT INTO `kelompok_kelas` (`id_kelompok_kelas`, `nama_kelompok_kelas`) VALUES
+(1, 'RPL'),
+(2, 'TKJ');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kurikulum`
+--
+
+CREATE TABLE `kurikulum` (
+  `id_kurikulum` int(11) NOT NULL,
+  `nama_kurikulum` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kurikulum`
+--
+
+INSERT INTO `kurikulum` (`id_kurikulum`, `nama_kurikulum`) VALUES
+(1, 'K13');
 
 -- --------------------------------------------------------
 
@@ -93,9 +147,19 @@ CREATE TABLE `log` (
 
 CREATE TABLE `mapel` (
   `id_mapel` int(11) NOT NULL,
+  `id_kurikulum` int(11) NOT NULL,
   `nama_mapel` varchar(50) NOT NULL,
   `kkm` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mapel`
+--
+
+INSERT INTO `mapel` (`id_mapel`, `id_kurikulum`, `nama_mapel`, `kkm`) VALUES
+(1, 1, 'Matematika', 75),
+(2, 1, 'Bahasa Inggris', 75),
+(5, 1, 'Bahasa Indonesia', 75);
 
 -- --------------------------------------------------------
 
@@ -106,9 +170,17 @@ CREATE TABLE `mapel` (
 CREATE TABLE `mapel_guru` (
   `id_mapel_guru` int(11) NOT NULL,
   `id_mapel` int(11) DEFAULT NULL,
-  `id_guru` int(11) DEFAULT NULL,
-  `jml_kd` int(2) NOT NULL
+  `id_guru` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mapel_guru`
+--
+
+INSERT INTO `mapel_guru` (`id_mapel_guru`, `id_mapel`, `id_guru`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -122,6 +194,16 @@ CREATE TABLE `mapel_kelas` (
   `id_kelas` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `mapel_kelas`
+--
+
+INSERT INTO `mapel_kelas` (`id_mapel_kelas`, `id_mapel`, `id_kelas`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 1),
+(4, 5, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -132,13 +214,23 @@ CREATE TABLE `nilai` (
   `id_nilai` int(11) NOT NULL,
   `id_siswa` int(11) DEFAULT NULL,
   `id_mapel` int(11) DEFAULT NULL,
-  `nilai_kd` int(11) NOT NULL,
-  `nilai_uts` int(11) NOT NULL,
-  `nilai_uas` int(11) NOT NULL,
   `nilai_sikap` int(11) NOT NULL,
   `nilai_akhir` int(11) NOT NULL,
   `kelas` text NOT NULL,
   `th_ajar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai_kd`
+--
+
+CREATE TABLE `nilai_kd` (
+  `id_nilai_kd` int(11) NOT NULL,
+  `id_nilai` int(11) NOT NULL,
+  `id_kd` int(11) NOT NULL,
+  `nilai` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -151,6 +243,20 @@ CREATE TABLE `rumus` (
   `nilai_kd` int(11) NOT NULL,
   `nilai_uts` int(11) NOT NULL,
   `nilai_uas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `semester`
+--
+
+CREATE TABLE `semester` (
+  `id_semester` int(11) NOT NULL,
+  `id_nilai` int(11) NOT NULL,
+  `nilai_uts` int(11) NOT NULL,
+  `nilai_uas` int(11) NOT NULL,
+  `urutan` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -179,7 +285,8 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`id_siswa`, `nisn`, `nis`, `nama_siswa`, `tempat_lahir`, `tgl_lahir`, `jk`, `foto_siswa`, `id_kelas`, `id_guru`, `password`, `th_kelulusan`) VALUES
-(1, 'nisn1234', 'nis123', 'Abc Def', 'Malang', '2000-12-04', 1, NULL, NULL, NULL, 'nisn1234', NULL);
+(1, 'nisn1', 'nis1', 'Abc Def', 'Malang', '2000-12-04', 1, NULL, 1, NULL, 'nisn1', NULL),
+(2, 'nisn2', 'nis2', 'Ghi Jkl', 'Surabaya', '1999-12-28', 0, NULL, 2, NULL, 'nisn2', NULL);
 
 --
 -- Indexes for dumped tables
@@ -198,23 +305,37 @@ ALTER TABLE `guru`
   ADD PRIMARY KEY (`id_guru`);
 
 --
+-- Indexes for table `kd`
+--
+ALTER TABLE `kd`
+  ADD PRIMARY KEY (`id_kd`),
+  ADD KEY `id_mapel` (`id_mapel`);
+
+--
 -- Indexes for table `kelas`
 --
 ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`id_kelas`);
+  ADD PRIMARY KEY (`id_kelas`),
+  ADD KEY `id_kelompok_kelas` (`id_kelompok_kelas`);
 
 --
--- Indexes for table `log`
+-- Indexes for table `kelompok_kelas`
 --
-ALTER TABLE `log`
-  ADD PRIMARY KEY (`id_log`),
-  ADD KEY `id_guru` (`id_guru`);
+ALTER TABLE `kelompok_kelas`
+  ADD PRIMARY KEY (`id_kelompok_kelas`);
+
+--
+-- Indexes for table `kurikulum`
+--
+ALTER TABLE `kurikulum`
+  ADD PRIMARY KEY (`id_kurikulum`);
 
 --
 -- Indexes for table `mapel`
 --
 ALTER TABLE `mapel`
-  ADD PRIMARY KEY (`id_mapel`);
+  ADD PRIMARY KEY (`id_mapel`),
+  ADD KEY `id_kurikulum` (`id_kurikulum`);
 
 --
 -- Indexes for table `mapel_guru`
@@ -241,6 +362,21 @@ ALTER TABLE `nilai`
   ADD KEY `id_mapel` (`id_mapel`);
 
 --
+-- Indexes for table `nilai_kd`
+--
+ALTER TABLE `nilai_kd`
+  ADD PRIMARY KEY (`id_nilai_kd`),
+  ADD KEY `id_nilai` (`id_nilai`),
+  ADD KEY `id_kd` (`id_kd`);
+
+--
+-- Indexes for table `semester`
+--
+ALTER TABLE `semester`
+  ADD PRIMARY KEY (`id_semester`),
+  ADD KEY `id_nilai` (`id_nilai`);
+
+--
 -- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
@@ -261,51 +397,83 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `guru`
 --
 ALTER TABLE `guru`
-  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `kd`
+--
+ALTER TABLE `kd`
+  MODIFY `id_kd` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `log`
+-- AUTO_INCREMENT for table `kelompok_kelas`
 --
-ALTER TABLE `log`
-  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `kelompok_kelas`
+  MODIFY `id_kelompok_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `kurikulum`
+--
+ALTER TABLE `kurikulum`
+  MODIFY `id_kurikulum` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `mapel_guru`
 --
 ALTER TABLE `mapel_guru`
-  MODIFY `id_mapel_guru` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mapel_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `mapel_kelas`
 --
 ALTER TABLE `mapel_kelas`
-  MODIFY `id_mapel_kelas` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mapel_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
   MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `nilai_kd`
+--
+ALTER TABLE `nilai_kd`
+  MODIFY `id_nilai_kd` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `semester`
+--
+ALTER TABLE `semester`
+  MODIFY `id_semester` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Ketidakleluasaan untuk tabel `log`
+-- Ketidakleluasaan untuk tabel `kd`
 --
-ALTER TABLE `log`
-  ADD CONSTRAINT `log_ibfk_1` FOREIGN KEY (`id_guru`) REFERENCES `guru` (`id_guru`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `kd`
+  ADD CONSTRAINT `kd_ibfk_1` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`id_mapel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `kelas`
+--
+ALTER TABLE `kelas`
+  ADD CONSTRAINT `kelas_ibfk_1` FOREIGN KEY (`id_kelompok_kelas`) REFERENCES `kelompok_kelas` (`id_kelompok_kelas`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `mapel`
+--
+ALTER TABLE `mapel`
+  ADD CONSTRAINT `mapel_ibfk_1` FOREIGN KEY (`id_kurikulum`) REFERENCES `kurikulum` (`id_kurikulum`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `mapel_guru`
@@ -327,6 +495,19 @@ ALTER TABLE `mapel_kelas`
 ALTER TABLE `nilai`
   ADD CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`id_mapel`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `nilai_ibfk_2` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `nilai_kd`
+--
+ALTER TABLE `nilai_kd`
+  ADD CONSTRAINT `nilai_kd_ibfk_1` FOREIGN KEY (`id_nilai`) REFERENCES `nilai` (`id_nilai`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `nilai_kd_ibfk_2` FOREIGN KEY (`id_kd`) REFERENCES `kd` (`id_kd`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `semester`
+--
+ALTER TABLE `semester`
+  ADD CONSTRAINT `semester_ibfk_1` FOREIGN KEY (`id_nilai`) REFERENCES `nilai` (`id_nilai`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `siswa`
