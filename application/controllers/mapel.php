8d30c3dc61dd8f17bfa497efbfb68098827be380
sitiafiyah <sitiafiyah35@gmail.com>
2017-12-19 20:51:59 +0700

    Display, Add & Update Mapel

--- application/controllers/mapel.php
+++ application/controllers/mapel.php
@@ -14,7 +14,9 @@ class Mapel extends CI_Controller {
 
 	public function index()
 	{
-		
+		$data['title'] = 'Mapel';
+		$data['main'] = 'admin/mapel/index';
+		$this->load->view('template', $data);
 	}
 
 	public function getMapelGuru()
@@ -59,6 +61,107 @@ class Mapel extends CI_Controller {
 		}
 
 		echo json_encode($r);
+	}
+
+	public function addMapel()
+	{
+		if(! $this->input->is_ajax_request()) {
+			$data['title'] = '404 Page Not Found';
+    		$this->load->view('error404_view',$data);
+    		return;
+		}
+
+		$r = array('status' => false, 'error' => '');
+		$data = array(
+			//'id_mapel'		=> $this->input->post('id_mapel'),
+			'id_kurikulum'	=> $this->input->post('id_kurikulum'),
+			'nama_mapel'	=> $this->input->post('nama_mapel'),
+			'kkm'			=> $this->input->post('kkm')
+		);
+
+		if (
+			$data['nama_mapel'] == '' ||
+			$data['kkm'] == '')
+		{
+			$r['error'] = 'Isi semua data yang diperlukan!';
+			echo json_encode($r);
+			return;
+		}
+
+		if ($this->input->post('kurikulum')) {
+			$data['id_kurikulum'] = $this->input->post('kurikulum');
+		}
+
+		//$data = $this->service_model->escape_array($data);
+		if ($this->mapel_model->addMapel($data)) {
+			$r['status'] = true;
+		} else {
+			$r['error'] = 'Gagal menambahkan mapel';
+		}
+
+		echo json_encode($r);
+	}
+
+	public function getAllMapel()
+	{
+		if(! $this->input->is_ajax_request()) {
+			$data['title'] = '404 Page Not Found';
+    		$this->load->view('error404_view',$data);
+    		return;
+		}
+
+		$data = $this->mapel_model->getAllMapel();
+		$r = array('data' => $data );
+
+		echo json_encode($r);
+	}
+
+	public function editMapel()
+	{
+		if(! $this->input->is_ajax_request()) {
+			$data['title'] = '404 Page Not Found';
+    		$this->load->view('error404_view',$data);
+    		return;
+		}
+
+		$r = array('status' => false, 'error' => '');
+		$data = array(
+			'id_kurikulum' => $this->input->post('kurikulum'),
+			'nama_mapel' => $this->input->post('nama_mapel'),
+			'kkm' => $this->input->post('kkm')
+		);
+
+		if ($data['nama_mapel'] == '' || $data['kkm'] == '') {
+			$r['error'] = 'Isi semua data yang diperlukan!';
+			echo json_encode($r);
+			return;
+		}
+		if ($this->input->post('kurikulum')) {
+			$data['id_kurikulum'] = $this->input->post('kurikulum');
+		} else {
+			$data['id_kurikulum'] = null;
+		}
+
+		if ($this->mapel_model->editMapel($this->input->post('id'), $data)) {
+			$r['status'] =true;
+		} else {
+			$r['error'] = 'Gagal mengedit mapel';
+		}
+
+		echo json_encode($r);
+	}
+
+	public function getMapelById()
+	{
+		if(! $this->input->is_ajax_request()) {
+			$data['title'] = '404 Page Not Found';
+    		$this->load->view('error404_view',$data);
+    		return;
+		}
+
+		$r = $this->mapel_model->getMapelById($this->input->get('id'));
+
+		echo json_encode($r[0]);
 	}
 
 }
---
+++ application/views/admin/mapel/modal_body.php
@@ -0,0 +1,18 @@
+<div class="row">\
+  <div class="col-lg-6">\
+    <div class="form-group">\
+      <label>Kurikulum</label>\
+      <select name="kurikulum" class="form-control select2" id="kurikulum">\
+        <option value="">-Pilih kurikulum-</option>\
+      </select>\
+    </div>\
+    <div class="form-group">\
+      <label>Nama Mapel</label>\
+      <input type="text" name="nama_mapel" class="form-control" id="nama_mapel">\
+    </div>\
+    <div class="form-group">\
+      <label>KKM</label>\
+      <input type="text" name="kkm" class="form-control" id="kkm">\
+    </div>\
+  </div>\
+</div>
